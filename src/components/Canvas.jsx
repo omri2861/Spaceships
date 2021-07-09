3cf10871079e93deb55182676a72ccedd68e0ee5
Omri Levy <omri2861@gmail.com>
2021-07-09 16:17:32 +0300

    Added some assets and a Toucan node

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactFlow, { Handle } from "react-flow-renderer";
 import { makeStyles } from "@material-ui/core";
+import Toucan from "../assets/toucan.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,35 +13,35 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const elements = [
-  { id: "1", data: {text: "This is some example text"}, position: { x: 250, y: 5 }, type: "special"},
+  {
+    id: "1",
+    data: { text: "Toucan" },
+    position: { x: 250, y: 5 },
+    type: "special",
+  },
   // you can also pass a React component as a label
   { id: "2", data: { label: <div>Node 2</div> }, position: { x: 100, y: 100 } },
   { id: "e1-2", source: "1", target: "2", animated: true },
 ];
 
 const customNodeStyles = {
-  background: "#9CA8B3",
-  color: "#FFF",
+  color: "black",
+  fontSize: "16px",
   padding: 10,
+  width: "50px",
+};
+
+const imgStyle = {
+  width: "100%"
 };
 
 const CustomNodeComponent = ({ data }) => {
   return (
     <div style={customNodeStyles}>
       <Handle type="target" position="left" style={{ borderRadius: 0 }} />
-      <div>{data.text}</div>
-      <Handle
-        type="source"
-        position="right"
-        id="a"
-        style={{ top: "30%", borderRadius: 0 }}
-      />
-      <Handle
-        type="source"
-        position="right"
-        id="b"
-        style={{ top: "70%", borderRadius: 0 }}
-      />
+      <img src={Toucan} alt="Toucan" style={imgStyle} />
+      <p>{data.text}</p>
+      <Handle type="source" position="right" style={{ borderRadius: 0 }} />
     </div>
   );
 };
@@ -53,7 +54,7 @@ export default function Canvas() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <ReactFlow elements={elements} nodeTypes={nodeTypes}/>
+      <ReactFlow elements={elements} nodeTypes={nodeTypes} />
     </div>
   );
 }
