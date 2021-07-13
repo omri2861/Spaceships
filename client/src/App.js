467d08d4a040db2d6e7d90ca6d068f878773b707
Omri Levy <omri2861@gmail.com>
2021-07-13 23:11:51 +0300

    Copied snackbar code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Map from "./components/Map";
 import Bar from "./components/Bar";
+import Snackbar from "./components/Snackbar";
 
 function App() {
   return (
     <div>
       <Bar />
       <Map />
+      <Snackbar />
     </div>
   );
 }
