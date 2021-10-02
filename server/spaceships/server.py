440f48723ff9fe650ce354cdc19de713dea674e4
Omri Levy <omri2861@gmail.com>
2021-10-02 03:49:10 +0300

    Added teardown for the mongo client context

--- server/spaceships/server.py
+++ server/spaceships/server.py
@@ -31,6 +31,14 @@ def get_functions():
     return g.db[DB_NAME]["Functions"]
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
